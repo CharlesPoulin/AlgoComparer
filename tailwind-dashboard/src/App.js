82d21850ad751a31ae30419ebdd99d854d5bde4a
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2023-11-05 18:54:20 -0500

    first commit

---
+++ tailwind-dashboard/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './App.css'; // Make sure this is imported
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your dashboard code will go here */}
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
